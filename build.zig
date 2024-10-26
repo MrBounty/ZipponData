dcb72ff22afd3024edad7125921655e44e962b2f
MrBounty <adrien.bouvais.pro@gmail.com>
2024-10-26 12:37:35 +0200

    New build

--- build.zig
+++ build.zig
@@ -1,12 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
     _ = b.addModule("ZipponData", .{
-        .root_source_file = b.path("main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_source_file = b.path("src/main.zig"),
     });
 }
