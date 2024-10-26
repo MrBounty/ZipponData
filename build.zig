b329018a5fbfad39377b3414e4e5c5ef0591df6c
MrBounty <adrien.bouvais.pro@gmail.com>
2024-10-26 11:58:34 +0200

    Updated build and build.zon

--- build.zig
+++ build.zig
@@ -4,10 +4,9 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
 
-    const dep_opts = .{ .target = target, .optimize = optimize };
-    _ = dep_opts;
-
     _ = b.addModule("ZipponData", .{
         .root_source_file = b.path("main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 }
